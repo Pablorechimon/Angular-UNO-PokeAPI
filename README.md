a2ae60111e55c63e8097f25e87174ccba7279900
Pablorechimon <pablo.rechimon@gmail.com>
2021-06-13 00:28:08 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-uno-pokeapi-pablorechimon-tsmhfw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-uno-pokeapi-pablorechimon-tsmhfw)
---
+++ src/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../Services/data.service';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.css']
+})
+export class PokemonListComponent implements OnInit {
+  pokemons: any[] = [];
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.getPokemons().subscribe((response: any) => {
+      response.results.forEach(result => {
+        this.dataService
+          .getMoreData(result.name)
+          .subscribe((uniqResponse: any) => {
+            this.pokemons.push(uniqResponse);
+            console.log(this.pokemons);
+          });
+      });
+    });
+  }
+}
